ecb5c2dc25d309273eafe15d913fd53a27ce42e9
MuellerNicolas <nicolas.moritz.mueller@web.de>
2020-04-24 21:31:50 +0200

    thread in sound

--- Alarm_Sound/alarm_sound.py
+++ Alarm_Sound/alarm_sound.py
@@ -1,9 +1,16 @@
+import threading
+
 class AlarmSound:
     def __init__(self, broker):
         self.broker = broker
         broker.subscribe("alarm-beep", self._beep)
         broker.subscribe("alarm-stop", self._stopAlarm)
         broker.subscribe("alarm-snooze", self._stopAlarm)
+
+    def _run(self):
+        self._thread_button_flag = threading.Event()
+        self._thread_button = threading.Thread(target= self._melody, name = 'voice-app-alarm-sound-thread', daemon = True)
+        self._thread_button.start()
 
     def close(self):
         pass
