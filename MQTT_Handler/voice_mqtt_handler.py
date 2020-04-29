b5a443180ce69e4b6faafbcad1cc4fa3a975bdfa
MuellerNicolas <nicolas.moritz.mueller@web.de>
2020-04-29 21:53:24 +0200

    added mqtt settings

--- MQTT_Handler/voice_mqtt_handler.py
+++ MQTT_Handler/voice_mqtt_handler.py
@@ -1,11 +1,21 @@
 from MQTT_Handler.mqtt_receiver import MQTTReceiver
 import traceback
 import logging
+import json
+import os
 
 class MQTTHandler:
     def __init__(self, broker):
+        # internal pubsub broker
         self._broker = broker
-        self._mqtt_receiver = MQTTReceiver(self._broker, '192.168.178.41', 1883)
+
+        # read mqtt setting 4 connection
+        path = os.path.join(os.path.dirname(__file__), 'mqtt_settings.json')
+        with open(path) as f:
+            mqtt_settings = json.load(f)
+        self._ip = mqtt_settings["ip"]
+        self._port = mqtt_settings["port"]
+        self._mqtt_receiver = MQTTReceiver(self._broker, self._ip, self._port)
         self._setup()
 
     def _setup(self):
